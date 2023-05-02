bc364308e79a9a5a75ace2e9f24a9289c8d7d7a1
EmranAli-15 <mohammademranali15@gmail.com>
2023-05-02 22:55:38 +0600

    hide firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBKjfEL8Hk-FJLAXyxXx4hMdne8asi_O2U",
-  authDomain: "chef-recipes-56877.firebaseapp.com",
-  projectId: "chef-recipes-56877",
-  storageBucket: "chef-recipes-56877.appspot.com",
-  messagingSenderId: "413854795382",
-  appId: "1:413854795382:web:29915ff75cd0a6a8cc0157"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
