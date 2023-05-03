b8ffafdd08002a8535e99e8450e6df2fb7e5b9e4
EmranAli-15 <mohammademranali15@gmail.com>
2023-05-04 05:08:57 +0600

    fixed a siily mistake

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
     const [error, setError] = useState('');
     const navigate = useNavigate();
     const location = useLocation();
-    const from = location.state?.from?.pathname || "/category/0";
+    const from = location.state?.from?.pathname || "/";
     const handleCreateUser = (event) => {
         event.preventDefault();
 
